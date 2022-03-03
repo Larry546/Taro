df2d3cb2c2924a953f02f1737c4c59217423e220
Larry <765892208@qq.com>
2022-03-03 22:01:53 +0800

    feat: orderList 初步框架

--- src/pages/order-list/index.tsx
+++ src/pages/order-list/index.tsx
@@ -1,5 +1,8 @@
 import PureComponent from "../../common/pure-component";
 import { View, Text } from "@tarojs/components";
+import { AtList, AtNavBar } from "taro-ui";
+import getEnv from "../../system/tools/environment";
+import { IOrderInfo } from "./interface";
 
 import "./index.scss";
 
@@ -8,6 +11,28 @@ definePageConfig({
 });
 
 export default class Index extends PureComponent<any> {
+    orderList: Array<IOrderInfo>;
+    constructor(props: any) {
+        super(props);
+        this.orderList = [
+            {
+                orderId: 1,
+                orderName: "绍兴柯岩风景区门票绍兴柯岩风景区门票",
+                orderTotal: 115,
+                orderStatus: "待支付",
+                orderCreateTime: "2022-03-03",
+                orderuseTime: "2022-03-31",
+            },
+            {
+                orderId: 1,
+                orderName: "绍兴柯岩风景区门票",
+                orderTotal: 115,
+                orderStatus: "待支付",
+                orderCreateTime: "2022-03-03",
+                orderuseTime: "2022-03-31",
+            },
+        ];
+    }
     componentWillMount() {}
 
     componentDidMount() {}
@@ -24,8 +49,40 @@ export default class Index extends PureComponent<any> {
 
     render() {
         return (
-            <View className="index">
-                <Text>订单列表页</Text>
+            <View className="orderlist">
+                {getEnv() === "H5" ? (
+                    <View className="orderlist_h5NavBar">
+                        <AtNavBar
+                            fixed={true}
+                            border={false}
+                            title={"订单列表"}
+                            onClickLeftIcon={this.pop}
+                            leftIconType={{ value: "left", prefixClass: "icon", color: "#000000" }}
+                        ></AtNavBar>
+                    </View>
+                ) : null}
+                {this.orderList.length ? (
+                    <View>
+                        <View>
+                            {this.orderList.map((item, value) => {
+                                // todo
+                                return (
+                                    <View>
+                                        <Text>{item.orderName}</Text>
+                                    </View>
+                                );
+                            })}
+                        </View>
+
+                        <View className="orderlist_end" style={{ height: "50px" }}>
+                            <Text>没有更多了</Text>
+                        </View>
+                    </View>
+                ) : (
+                    <View className="orderlist_end">
+                        <Text>没有订单</Text>
+                    </View>
+                )}
             </View>
         );
     }
---
+++ src/pages/order-list/interface.ts
@@ -0,0 +1,8 @@
+export interface IOrderInfo {
+    orderId: number;
+    orderName: string; //景点名+票名拼接
+    orderTotal: number;
+    orderStatus: string;
+    orderCreateTime: string;
+    orderuseTime: string;
+}
