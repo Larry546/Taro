8a2effe4d61d242d83f7f2aa0397120ef9119b7c
Larry <765892208@qq.com>
2022-03-17 21:48:26 +0800

    feat: 后台管理系统封装http请求

--- travel-manage/src/service/index.ts
+++ travel-manage/src/service/index.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { get, post } from "./tools";
 
 export const npmDependencies = () =>
     axios
--- travel-manage/src/service/tools.ts
+++ travel-manage/src/service/tools.ts
@@ -4,39 +4,38 @@
 import axios from "axios";
 import { message } from "antd";
 
-interface IFRequestParam {
-    url: string;
-    msg?: string;
-    config?: any;
-    data?: any;
-}
-/**
- * 公用get请求
- * @param url       接口地址
- * @param msg       接口异常提示
- * @param headers   接口所需header配置
- */
-export const get = ({ url, msg = "接口异常", config }: IFRequestParam) =>
-    axios
-        .get(url, config)
-        .then(res => res.data)
-        .catch(err => {
-            console.log(err);
-            message.warn(msg);
-        });
+const BASE_URL = "http://localhost:8088";
 
-/**
- * 公用post请求
- * @param url       接口地址
- * @param data      接口参数
- * @param msg       接口异常提示
- * @param headers   接口所需header配置
- */
-export const post = ({ url, data, msg = "接口异常", config }: IFRequestParam) =>
-    axios
-        .post(url, data, config)
-        .then(res => res.data)
-        .catch(err => {
-            console.log(err);
-            message.warn(msg);
-        });
+class httpRequest {
+    get(url: string, msg: string = "接口异常", config?: any) {
+        return axios
+            .get(BASE_URL + url, config)
+            .then(res => res.data)
+            .catch(err => {
+                console.log(err);
+                message.warn(msg);
+            });
+    }
+
+    post(url: string, data?: any, msg: string = "接口异常", config?: any) {
+        return axios
+            .post(BASE_URL + url, data, config)
+            .then(res => res.data)
+            .catch(err => {
+                console.log(err);
+                message.warn(msg);
+            });
+    }
+
+    delete(url: string, msg: string = "接口异常", config?: any) {
+        return axios
+            .delete(BASE_URL + url, config)
+            .then(res => res.data)
+            .catch(err => {
+                console.log(err);
+                message.warn(msg);
+            });
+    }
+}
+
+export default new httpRequest();
