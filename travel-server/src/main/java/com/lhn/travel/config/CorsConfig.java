522ac38b7ec4e7fa5c2b6d2c8beabd9f7bc72ac3
Larry <765892208@qq.com>
2022-03-23 20:16:04 +0800

    feat: 后端注释

--- travel-server/src/main/java/com/lhn/travel/config/CorsConfig.java
+++ travel-server/src/main/java/com/lhn/travel/config/CorsConfig.java
@@ -16,8 +16,8 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("http://localhost:8086"); // 1 设置访问源地址
-        corsConfiguration.addAllowedOrigin("http://localhost:8087");
+        corsConfiguration.addAllowedOrigin("http://localhost:8086"); // 1 设置访问源地址 客户端
+        corsConfiguration.addAllowedOrigin("http://localhost:8087"); // 管理员端
         corsConfiguration.addAllowedHeader("*"); // 2 设置访问源请求头
         corsConfiguration.addAllowedMethod("*"); // 3 设置访问源请求方法
         corsConfiguration.setMaxAge(MAX_AGE);
